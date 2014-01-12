0b5cd2145d2a5187c01ed53ea1ef4699f7a6c997
xternalx <i@xternalx.com>
2014-01-12 19:57:55 +0800

    Внесены правки для орисовки юзербара используя аргументы из запроса. Теперь из аргументов запроса выбирается скрипт пользователя для редактирования, и режим отрисовки - гиф, или пнг

--- index.php
+++ index.php
@@ -25,7 +25,15 @@ $engine = null;
 
 try
 {
-    $engine = new \DrawEngine\DrawEngine(file_get_contents("script.lua"), 350, 40, "silver");
+    if(!isset($_REQUEST['user']))
+        die("No user specified!");
+
+    $user = trim(str_replace("\0", "", $_REQUEST['user']));
+
+    if(!file_exists("users/".$user.".lua"))
+        die("Userscript was not found!");
+
+    $engine = new \DrawEngine\DrawEngine(file_get_contents("users/".$user.".lua"), 350, 40, "silver");
     //$engine->debugMode = true;
     $engine->registerMethods();
     $engine->loadPlugins();
@@ -38,11 +46,18 @@ try
 }
 $engine->debug("generated in: " . number_format(((microtime(true) - $start)), 3, '.', ' ') . " sec");
 
-if(isset($_REQUEST['gif']) && $_REQUEST['gif'])
+if (isset($_REQUEST['mode']))
 {
-$engine->renderAnim();
-}
-else
+    switch ($_REQUEST['mode'])
+    {
+        case "gif":
+            $engine->renderAnim();
+            break;
+        case "png":
+            $engine->render();
+            break;
+    }
+} else
 {
-$engine->render();
+    $engine->render();
 }
